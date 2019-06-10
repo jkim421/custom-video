67d267a2a5d573a2b67eb9bd775f472c18e29f86
john.kim <jkim21@pm.me>
2019-06-10 00:21:05 -0400

    Config

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class VideoPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Inside the Player</div>
+    )
+  }
+}
